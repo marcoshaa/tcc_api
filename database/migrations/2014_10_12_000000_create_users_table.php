91990d42a5b1698adda46da9e58bbe1deef012b5
Marcos <m.andrade.assis@gmail.com>
2022-07-21 01:14:16 -0300

    cad user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email',191)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('funcao',1);
             $table->rememberToken();
             $table->timestamps();
         });
